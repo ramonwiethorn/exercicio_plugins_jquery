f0b6076cfd5ddf288f802b38e735bb0ce39c5098
Ramon Wiethorn <ramonwiethorn@gmail.com>
2023-06-16 12:22:59 -0300

    correção na mascara CEP

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ $('#cpf').mask('000.000.000-00', {
     placeholder: '___.___.___-__'
 });
 $('#cep').mask('00.000-000', {
-    placeholder: '__.___-___'
+    placeholder: '_____-___'
 });
 
     $('form').validate({
